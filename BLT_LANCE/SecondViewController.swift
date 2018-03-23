c4cdb5b3fd45799778b32ca1c9686e9c75178a22
我 <lanceren@lancedemacbook-pro.local>
2018-03-23 22:43:11 +0800

    Initial Commit

---
+++ BLT_LANCE/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BLT_LANCE
+//
+//  Created by lance ren on 2018/3/23.
+//  Copyright © 2018年 lance ren. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
